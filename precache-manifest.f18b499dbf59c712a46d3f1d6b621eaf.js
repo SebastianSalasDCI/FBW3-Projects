766409026d808aa0062461c6b7d140661ef3fb37
SebastianSalasDCI <sebastian.salas.laurens@digitalcareerinstitute.org>
2020-07-01 01:20:21 +0200

    fix to deploy

---
+++ precache-manifest.f18b499dbf59c712a46d3f1d6b621eaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99e8045366d6ed96ef5bcf59d83b5c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b0826bd47300452d22",
+    "url": "/static/css/2.214ad93a.chunk.css"
+  },
+  {
+    "revision": "4b5da76f36396e7ad230",
+    "url": "/static/css/main.51a55a4e.chunk.css"
+  },
+  {
+    "revision": "98b0826bd47300452d22",
+    "url": "/static/js/2.d8fba6e2.chunk.js"
+  },
+  {
+    "revision": "bc1a73d08d8433691746793d8481dea3",
+    "url": "/static/js/2.d8fba6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b5da76f36396e7ad230",
+    "url": "/static/js/main.bc67e4b9.chunk.js"
+  },
+  {
+    "revision": "65f8c86f4c8250bd2868",
+    "url": "/static/js/runtime-main.5cdce585.js"
+  }
+]);
